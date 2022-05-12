040e6c9681ba656c59f55947a3f3f0195246166f
croso1024 <croso1024@gmail.com>
2022-05-12 19:55:03 +0800

    reviseSample_create

--- Benchmarker/createSample.py
+++ Benchmarker/createSample.py
@@ -1,15 +1,30 @@
 import numpy  as np 
 import json
 
-with open("map/expo_park.json","r") as file: 
-    data = json.load(file)
-location = list(data["goals"].keys())
-print(location)
-matrix = np.random.randint(low=0,high=3,size=(2,len(location)))
-adj = np.dot(matrix.transpose(),matrix)
-print(matrix)
-print(adj)
-adj = adj.tolist()
-with open("Adjency2.json","w") as file:
-    data = {"station":location,"adjencyMatrix":adj}    
-    file.write(json.dumps(data))
+
+
+def createFromJSON(input_path,out_path):  
+    #with open("map/expo_park.json","r") as file: 
+    with open(input_path,"r") as file : 
+        data = json.load(file)
+    location = list(data["goals"].keys())
+    print(location)
+    matrix = np.random.randint(low=0,high=3,size=(2,len(location)))
+    adj = np.dot(matrix.transpose(),matrix)
+    print(matrix)
+    print(adj)
+    adj = adj.tolist()
+    with open(out_path,"w") as file:
+        data = {"station":location,"adjencyMatrix":adj}    
+        file.write(json.dumps(data))
+        
+def createFromText(size: int , out_path): 
+    dot  = ["ABCDEFGHIJKLMNOPQRSTUV"] 
+    location ,length= dot[:size] , len(dot[:size])
+    matrix = np.random.randint(low=0,high = 10 , size= (3,length))
+    adj = np.dot(matrix.transpose(),matrix)
+    adj = adj.tolist() 
+    with open(out_path,"w") as file:
+        data = {"station":location , "adjencyMatrix":adj}
+        file.write(json.dumps(data))
+        
