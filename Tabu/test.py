604933a84b53df36c15f9f8201369cae686991bc
croso1024 <croso1024@gmail.com>
2022-06-30 12:11:30 +0800

    0630

--- Tabu/test.py
+++ Tabu/test.py
@@ -1,6 +1,9 @@
-for i in range(5): 
-    for j in range(20): 
-        print(j) 
-        if j ==5 : 
-            break
-    print("########")    
+import numpy as np 
+
+a = np.random.randint(low=0,high=100,size=(5,5))
+print(a)
+print("---------")
+print(a[2:4,2:4])          
+
+b = []
+b[0] =1 
